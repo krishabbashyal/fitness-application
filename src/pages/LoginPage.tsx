227e948c76b6c08446f994da372bd4277ddad5ca
Krishab Bashyal <krishab2002@gmail.com>
2023-09-08 16:43:01 -0500

    added new form fields and styles

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -14,9 +14,9 @@ function LoginPage() {
     <div className="text-center justify-center align-middle mt-24 mx-4">
       <h1 className="mb-4">Log in to your account</h1>
       <form action="" className="flex flex-col space-y-2">
-        <input type="email" placeholder="Email" onChange={event => setEmail(event.target.value)} className="p-3 border border-black h-10" />
-        <input type="password" placeholder="Password" onChange={event => setPassword(event.target.value)} className="p-3 border border-black h-10" />
-        <button onClick={submitHandler} className="p-4 bg-blue-200 mt-3">Login</button>
+        <input type="email" placeholder="Email" onChange={event => setEmail(event.target.value)} className=" rounded-md p-3 border border-black h-10" />
+        <input type="password" placeholder="Password" onChange={event => setPassword(event.target.value)} className=" rounded-md p-3 border border-black h-10" />
+        <button onClick={submitHandler} className=" rounded-md p-4 bg-blue-200 mt-3">Login</button>
       </form>
       <div className="mt-16">
         <Link to={"/register"}>Register Instead</Link>
--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -3,11 +3,13 @@ import { useState } from "react";
 
 function RegisterPage() {
   const [email, setEmail] = useState('')
+  const [name, setName] = useState('')
   const [password, setPassword] = useState('')
   const [confirmPassword, setConfirmPassword] = useState('')
 
   const submitHandler = () => {
     console.log(email)
+    console.log(name)
     console.log(password)
     console.log(confirmPassword)
   }
@@ -17,10 +19,11 @@ function RegisterPage() {
     <div className="text-center justify-center align-middle mt-24 mx-4">
       <h1 className="mb-4">Register a new account </h1>
       <form action="" className="flex flex-col space-y-2">
-        <input type="email" placeholder="Email" onChange={event => setEmail(event.target.value)} className="p-3 border border-black h-10" />
-        <input type="password" placeholder="Password" onChange={event => setPassword(event.target.value)} className="p-3 border border-black h-10" />
-        <input type="password" placeholder="Confirm password" onChange={event => setConfirmPassword(event.target.value)} className="p-3 border border-black h-10" />
-        <button onClick={submitHandler} className="p-4 bg-blue-200 mt-3">Register</button>
+        <input type="email" placeholder="Email" onChange={event => setEmail(event.target.value)} className="rounded-md p-3 border border-black h-10" />
+        <input type="text" placeholder="Display Name" onChange={event => setName(event.target.value)} className="rounded-md p-3 border border-black h-10" />
+        <input type="password" placeholder="Password" onChange={event => setPassword(event.target.value)} className="rounded-md p-3 border border-black h-10" />
+        <input type="password" placeholder="Confirm password" onChange={event => setConfirmPassword(event.target.value)} className="rounded-md p-3 border border-black h-10" />
+        <button onClick={submitHandler} className="rounded-md p-4 bg-blue-200 mt-3">Register</button>
       </form>
       <div className="mt-16">
         <Link to={"/login"}>Login Instead</Link>
