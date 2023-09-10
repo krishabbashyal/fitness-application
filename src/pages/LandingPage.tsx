4f02f2a4cb61755d3b204ced8f4f89d06622c041
Krishab Bashyal <krishab2002@gmail.com>
2023-09-09 22:09:12 -0500

    added redirects and signout

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,7 +1,12 @@
 import { Link } from "react-router-dom";
 
 function LandingPage() {
-  return <Link to={"/register"}>There is nothing here, click this instead</Link>;
+  return (
+    <div className="text-center justify-center align-middle mt-24 mx-4">
+      <p>Welcome to the fitness app landing page</p>
+      <p>There is not much here, click <span className="text-blue-600"><Link to={"/register"}>this</Link></span> instead.</p>
+    </div>
+  );
 }
 
 export default LandingPage;
