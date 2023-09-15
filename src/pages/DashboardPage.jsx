b6db8dc629eef45df48a291adec2aecd90e9f97c
Krishab Bashyal <krishab2002@gmail.com>
2023-09-14 22:19:09 -0500

    refactored dashboard page to use supabase

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -1,16 +1,20 @@
 import { useNavigate } from "react-router-dom";
+import { supabase } from "../library/supabase";
 
 function DashboardPage() {
   const customRedirect = useNavigate();
 
   const handleSignOut = async () => {
-    signOut(auth);
-    console.log("Signed Out");
-    customRedirect("/");
+    let { error } = await supabase.auth.signOut();
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Signed Out");
+      customRedirect("/");
+    }
   };
   return (
     <div className="text-center justify-center align-middle mt-24 mx-4">
-      <p>Hi, {auth.currentUser?.email}.</p>
       <button onClick={handleSignOut}>Sign Out</button>
     </div>
   );
