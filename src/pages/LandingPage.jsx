340213e2e97c867062d4cef663a16d98f2b39083
Krishab Bashyal <krishab2002@gmail.com>
2023-10-07 16:12:07 -0500

    wip form validation, added global background color

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,30 +1,12 @@
 import { Link } from "react-router-dom";
-import { supabase } from "../library/supabase";
-
-import { useEffect, useState } from "react";
-
 
 function LandingPage() {
-  const [countries, setCountries] = useState([]);
 
-  useEffect(() => {
-    getCountries();
-  }, []);
-
-  async function getCountries() {
-    console.log("getting data")
-    const { data } = await supabase.from("countries").select();
-    setCountries(data);
-  }
 
   return (
     <div>
-      <p>There is not much here, click <span className="text-blue-600"><Link to={"/register"}>this</Link></span> instead.</p>
-      <ul>
-        {countries.map((country) => (
-          <li key={country.name}>{country.name}</li>
-        ))}
-      </ul>
+      <p class="mt-24">There is not much here, click <button className="text-blue-600 text-2xl p-4"><Link to={"/register"}>this</Link></button> instead.</p>
+      <p class="mt-24">To Do: See if we can store the user object in the browser local storage or some other type of cache</p>
     </div>
   );
 }
--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -6,7 +6,22 @@ function RegisterPage() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
+
+  const [emailError, setEmailError] = useState(false)
+  const [passwordError, setPasswordError] = useState(false)
+  const [confirmError, setConfirmError] = useState(false)
+
   const customRedirect = useNavigate();
+
+  const toggleErrors = (event) => {
+    event.preventDefault()
+    setEmailError(!emailError)
+    console.log(emailError)
+    setPasswordError(!passwordError)
+    console.log(passwordError)
+    setConfirmError(!confirmError)
+    console.log(confirmError)
+  }
 
   const submitHandler = async (event) => {
     event.preventDefault();
@@ -30,33 +45,41 @@ function RegisterPage() {
     <div className="text-center justify-center align-middle mt-32 mx-4 max-w-lg">
       <h1 className="mb-6 text-[32px] font-semibold">Register</h1>
       <form>
-        <div className="flex flex-col space-y-3">
+        <div className="flex flex-col">
           <input
             type="email"
             placeholder="Email"
             className="rounded-lg pl-3 border placeholder:font-medium border-[#E8ECF4] h-14"
             onChange={(event) => setEmail(event.target.value)}
           />
+          { emailError ? <p className='text-left mt-1.5 text-[#991B1B]'>Please enter a valid email address</p> : null }
           <input
             type="password"
             placeholder="Password"
-            className="rounded-lg pl-3 border placeholder:font-medium border-[#E8ECF4] h-14"
+            className="rounded-lg pl-3 border placeholder:font-medium border-[#E8ECF4] h-14 mt-3"
             onChange={(event) => setPassword(event.target.value)}
           />
+          { passwordError ? <p className='text-left mt-1.5 text-[#991B1B]'>Password should be at least 6 characters</p> : null }
+          
           <input
             type="password"
             placeholder="Confirm password"
-            className="rounded-lg pl-3 border placeholder:font-medium border-[#E8ECF4] h-14 mb-4"
+            className="rounded-lg pl-3 border placeholder:font-medium border-[#E8ECF4] h-14 mt-3"
             onChange={(event) => setConfirmPassword(event.target.value)}
           />
-
-          <button onClick={submitHandler} className="rounded-md p-4 bg-[#475E88] font-semibold text-white">
+          { passwordError ? <p className='text-left mt-1.5 text-[#991B1B]'>Passwords do not match</p> : null }
+          
+          <button onClick={submitHandler} className="rounded-md p-4 mt-5 bg-[#475E88] font-semibold text-white">
             Register
           </button>
+          <button onClick={toggleErrors} className="rounded-md p-4 bg-red-900 font-semibold text-white mt-9">
+            Toggle errors
+          </button>
+          
         </div>
         <div className="mt-16 flex justify-center space-x-1">
-          <p class="text-[#1E232C] font-medium">Already have an account?</p>
-          <Link class="text-[#35C2C1] font-medium" to={"/login"}>
+          <p className="text-[#1E232C] font-medium">Already have an account?</p>
+          <Link className="text-[#35C2C1] font-medium" to={"/login"}>
             Login Instead
           </Link>
         </div>
