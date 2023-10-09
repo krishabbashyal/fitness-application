85b23c06d377e4c99f0fff990079a8dbf8b0e824
Krishab Bashyal <krishab2002@gmail.com>
2023-10-09 15:51:06 -0500

    implemented form validation

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -23,10 +23,26 @@ function LoginPage() {
   }
 
   const validateForm = (event) => {
-    console.log("Validating Form")
-    // If form is valid call submitHandler()
-  }
+    event.preventDefault()
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const validEmail = emailPattern.test(email)
 
+    if (validEmail) {
+      setEmailError(false)
+    } else {
+      setEmailError(true)
+    }
+
+    if (password.length >= 6) {
+      setPasswordError(false)
+    } else {
+      setPasswordError(true)
+    } 
+
+    if (!emailError && !passwordError) {
+      submitHandler(event)
+    }
+  }
   const submitHandler = async (event) => {
     event.preventDefault();
     let { data, error } = await supabase.auth.signInWithPassword({
@@ -60,14 +76,14 @@ function LoginPage() {
             className={`rounded-lg pl-3 placeholder:font-medium h-14 ${passwordError ? 'border-[#EF4444] border-2 placeholder:text-[#991B1B]' : 'border-[#E8ECF4] border mb-3'}`}
             onChange={(event) => setPassword(event.target.value)}
           />
-          { passwordError ? <p className='text-left mt-1 mb-2.5 text-[#991B1B]'>Password field cannot be empty</p> : null }
+          { passwordError ? <p className='text-left mt-1 mb-2.5 text-[#991B1B]'>Password should be at least 6 characters</p> : null }
       
           <div className="flex flex-row justify-end">
             <p className="mb-12 mt-4 text-[#35C2C1] font-semibold">
               <a href="">Forgot Password?</a>
             </p>
           </div>
-          <button onClick={submitHandler} className="rounded-md p-4 bg-[#475E88] font-semibold text-white mt-3">
+          <button onClick={validateForm} className="rounded-md p-4 bg-[#475E88] font-semibold text-white mt-3">
             Login
           </button>
           <button onClick={toggleErrors} className="rounded-md p-4 bg-red-900 font-semibold text-white mt-9">
