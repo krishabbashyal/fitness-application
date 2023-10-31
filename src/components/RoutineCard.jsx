1594d00792c82a0d753cf51fe552fe422f68a116
Krishab Bashyal <krishab2002@gmail.com>
2023-10-31 12:35:51 -0500

    completed the visuals of the RoutineCard

--- src/components/RoutineCard.jsx
+++ src/components/RoutineCard.jsx
@@ -5,16 +5,17 @@ const RoutineCard = () => {
     <div className="bg-[#475E88] text-white h-[142px] w-full px-4 rounded-lg">
       <div>
         <h1 className="text-3xl font-semibold pt-6">Workout Name</h1>
-        <p>Muscle One, Muscle Two</p>
+        <p className="-mt-1.5">Muscle One, Muscle Two</p>
       </div>
       <div className="mt-5 flex justify-between">
         <div className="flex space-x-2">
-          <img src="./public/icons/timeCircle.svg" alt="Time Circle" />
-          <p className="absolute pl-5">40 Mins</p>
+          <img className="w-6" src="./icons/timeCircle.svg" alt="Time Circle" />
+          <p className="text-sm mt-0.5">40 Mins</p>
         </div>
         <div>
-          <button>
-            <img src="./public/icons/Button.svg" alt="Time Circle" />
+          <button className="absolute right-10 top-[125px]">
+            <img className="absolute left-4 top-3" src="./icons/StartIcon.svg" alt="Time Circle" />
+            <img src="./icons/ButtonBackground.svg" alt="Time Circle" />
           </button>
         </div>
       </div>
