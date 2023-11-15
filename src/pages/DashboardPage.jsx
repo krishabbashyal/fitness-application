008e1a3e24540573cd773c27d7f2c6e425ad87b5
Krishab Bashyal <krishab2002@gmail.com>
2023-11-15 13:45:57 -0600

    completed the profile page

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -1,5 +1,5 @@
-import { useNavigate } from "react-router-dom";
-import { supabase } from "../library/supabase";
+// import { useNavigate } from "react-router-dom";
+// import { supabase } from "../library/supabase";
 import GreetingBar from "../components/GreetingBar";
 import RoutineCard from "../components/RoutineCard";
 import NavBar from "../components/NavBar";
@@ -7,17 +7,8 @@ import SectionHeader from "../components/SectionHeader";
 import CategoryCardScroller from "../components/CategoryCardScroller";
 
 function DashboardPage() {
-  const customRedirect = useNavigate();
 
-  const handleSignOut = async () => {
-    let { error } = await supabase.auth.signOut();
-    if (error) {
-      console.log(error);
-    } else {
-      console.log("Signed Out");
-      customRedirect("/");
-    }
-  };
+
   return (
     <div>
       <div className="mx-4">
@@ -28,7 +19,6 @@ function DashboardPage() {
         <SectionHeader header="Browse new routines" seeAll={true}/>
         <CategoryCardScroller/>
         <SectionHeader header="Popular routines"/>
-        <button className="mt-52 rounded-md w-full p-5 bg-red-500 text-white"  onClick={handleSignOut}>Sign Out</button>
       </div>
         <NavBar activeButton="dashboard"/>
     </div>
