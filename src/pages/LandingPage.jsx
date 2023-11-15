542f53877d260b694148ea861009a2e0ad720ebe
Krishab Bashyal <krishab2002@gmail.com>
2023-11-15 14:41:14 -0600

    added not now to onboarding2 and start working on landing page

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -7,44 +7,40 @@ function LandingPage() {
 
   useEffect(() => {
     const handleRedirect = async () => {
-      const { data: { user } } = await supabase.auth.getUser();
-      if (!user) {
-        console.log("No logged in user");
-        return;
-      }
-  
-      const { data, error } = await supabase
-        .from("profiles")
-        .select("completed_onboarding");
-  
+      const { data, error } = await supabase.from("profiles").select("completed_onboarding");
+
       if (error) {
-        console.error('Error fetching profile data:', error);
+        console.error("Error fetching profile data:", error);
         return;
       }
-  
+
       if (data.length === 0) {
-        console.warn('No profile data found');
+        console.warn("No profile data found");
         return;
       }
-  
-      const redirectTo = data[0].completed_onboarding ? '/dashboard' : '/onboarding';
+
+      const redirectTo = data[0].completed_onboarding ? "/dashboard" : "/onboarding";
       customRedirect(redirectTo);
     };
-  
+
     handleRedirect();
   }, []);
-  
 
   return (
-    <div>
-      <p className="mt-24">
-        There is not much here, click{" "}
-        <Link to={"/register"} className="text-blue-600 text-2xl p-4">
-          this
-        </Link>{" "}
-        instead.
-      </p>
-      {/* TODO: Explore caching the user object in browser local storage or other caching mechanisms */}
+    <div className="mx-4">
+      <div className="flex w-full space-x-2 text-white font-semibold h-14 mt-24">
+      <button className="w-full bg-[#1E232C] rounded-lg" type="button">
+          <Link to={"/register"} className="w-full h-14">
+            <p>Register</p>
+          </Link>
+        </button>
+
+        <button className="w-full bg-[#475E88] rounded-lg" type="button">
+          <Link to={"/login"} className="w-full h-14">
+            <p>Login</p>
+          </Link>
+        </button>
+      </div>
     </div>
   );
 }
