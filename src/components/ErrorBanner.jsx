e5a30a1b9f3167296de0ed137d0d70e5fc6ad92e
Suman Subedi <ssubedi60@gmail.com>
2023-11-16 11:05:06 -0600

    worked on displaying errorMessage

--- src/components/ErrorBanner.jsx
+++ src/components/ErrorBanner.jsx
@@ -1,12 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 
-function ErrorBanner() {
+function ErrorBanner(props) {
   return (
     <div
       className=" flex justify-center items-center mb-3 rounded-md w-auto
      h-12 bg-[#FFB1B1] shrink-0 "
     >
-      <p className="text-[#991B1B]">User already registered</p>
+      <p className="text-[#991B1B]">{props.message}</p>
     </div>
   );
 }
