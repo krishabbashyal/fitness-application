e357edd77519a3c75e3b2676708c41c61e3acdc8
Krishab Bashyal <krishab2002@gmail.com>
2023-11-20 00:14:52 -0600

    landing page redesign

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -27,13 +27,14 @@ function LandingPage() {
   }, []);
 
   return (
-    <div className="mx-4">
-      <div className="flex w-full space-x-2 text-white font-semibold h-16 mt-2">
-        <button className="w-full mt-2 bg-[#008080] rounded-lg" type="button">
-          <Link to={"/login"} className="w-full h-14">
-            <p>Install Our App</p>
-          </Link>
-        </button>
+    <div className="mx-4 h-full">
+      <div className="mt-10">
+        <h1 className="text-[36px] font-semibold">Name.</h1>
+        <h2 className="text-2xl">For those who want to take fitness to the next level.</h2>
+      </div>
+      
+      <div className="flex justify-center my-4">
+        <img className="rounded-[24px] w-96" src="../public/images/HeroImage.png" alt="" />
       </div>
       <div className="flex w-full space-x-2 text-white font-semibold h-14 mt-2">
         <button className="w-full bg-[#1E232C] rounded-lg" type="button">
@@ -45,6 +46,29 @@ function LandingPage() {
         <button className="w-full bg-[#475E88] rounded-lg" type="button">
           <Link to={"/login"} className="w-full h-14">
             <p>Login</p>
+          </Link>
+        </button>
+      </div>
+      <div className="mt-4">
+        <p>Join our community today and start your journey towards a healthier, more active lifestyle!</p>
+      </div>
+
+      <div className="-mx-4 bg-[#1E232C] text-white text-center py-2 mt-5">
+        <div className="my-4 flex flex-col space-y-3 mx-4">
+          <p className="font-semibold text-xl">Track Workouts Easily</p>
+          <p className="font-light pb-3 border-b">Log gym sessions effortlessly, maintaining a consistent record of your fitness routine.</p>
+          <p className="font-semibold text-xl">Discover Insights</p>
+          <p className="font-light pb-3 border-b">Analyze your workout patterns with in-depth insights, helping you optimize your fitness strategy.</p>
+          <p className="font-semibold text-xl">Visualize Progress</p>
+          <p className="font-light pb-3 border-b">Watch as your body transforms and track your health improvements over time.</p>
+          <p className="font-semibold text-xl">Personalized Recommendations</p>
+          <p className="font-light">Get customized weight suggestions tailored to your unique workout history and progress.</p>
+        </div>
+      </div>
+      <div className="w-full flex text-white font-semibold my-4 h-14">
+        <button className="w-full mt-2 bg-[#008080] rounded-lg" type="button">
+          <Link to={"/login"} className="w-full h-14">
+            <p>Install Our App</p>
           </Link>
         </button>
       </div>
