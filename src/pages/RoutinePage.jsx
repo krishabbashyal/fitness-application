2eed678cd2d0ebaeeb2f049477cd91de55c07b92
Suman Subedi <ssubedi60@gmail.com>
2023-11-21 19:44:22 -0600

    Worked on RoutinePage

--- src/pages/RoutinePage.jsx
+++ src/pages/RoutinePage.jsx
@@ -3,10 +3,15 @@ import NavBar from "../components/NavBar";
 const RoutinePage = () => {
   return (
     <div>
-      <p>Routine Page</p>
+      <h1 className="text-2xl m-6">Your Routine</h1>
+        <p className=" flex justify-center m-6">When you create a routine it will show up here</p>
+        <div className="flex justify-center">
+        <button className="rounded-md p-4 bg-[#475E88] font-medium text-white">Create Routine</button>
+        </div>
       <NavBar activeButton="routines"/>
     </div>
+    
   )
 }
 
-export default RoutinePage
+export default RoutinePage;
