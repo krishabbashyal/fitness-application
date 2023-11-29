9c076c9d671ef9875ca93f66e835f5a294243e32
Krishab Bashyal <krishab2002@gmail.com>
2023-11-29 00:13:00 -0600

    merged into main

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -1,10 +1,7 @@
-// import { useNavigate } from "react-router-dom";
-// import { supabase } from "../library/supabase";
 import GreetingBar from "../components/GreetingBar";
 import RoutineCard from "../components/RoutineCard";
 import NavBar from "../components/NavBar";
 import SectionHeader from "../components/SectionHeader";
-import CategoryCardScroller from "../components/CategoryCardScroller";
 
 function DashboardPage() {
 
@@ -16,9 +13,7 @@ function DashboardPage() {
         <SectionHeader header="Today's workout"/>
         <RoutineCard/>
         <SectionHeader header="Your progress" seeAll={true}/>
-        <SectionHeader header="Browse new workouts" seeAll={true}/>
-        <CategoryCardScroller/>
-        
+         
       </div>
         <NavBar activeButton="dashboard"/>
     </div>
--- src/pages/RoutinePage.jsx
+++ src/pages/RoutinePage.jsx
@@ -1,17 +1,33 @@
 import NavBar from "../components/NavBar";
+import SectionHeader from "../components/SectionHeader"
+import CategoryCardScroller from "../components/CategoryCardScroller";
 
 const RoutinePage = () => {
   return (
     <div>
-      <h1 className="text-2xl m-6">Your Routine</h1>
-        <p className=" flex justify-center m-6">When you create a routine it will show up here</p>
-        <div className="flex justify-center">
-        <button className="rounded-md p-4 bg-[#475E88] font-medium text-white">Create Routine</button>
+      <div className="mx-4 mt-6">
+        <SectionHeader header="Your Workouts" />
+        <div  className=" flex flex-col justify-center space-y-4 m-6">
+          <p>When you create a routine it will show up here</p>
+          <button className="rounded-md p-4 bg-[#475E88] font-medium text-white">Create Workout</button>
         </div>
-      <NavBar activeButton="routines"/>
+        <div>
+          <section>
+            <h1 className="text-2xl">Chest Day</h1>
+            <ul>
+              <li>Bench Press</li>
+              <li>Workout 1</li>
+              <li>Workout 1</li>
+              <li>Workout 1</li>
+            </ul>
+          </section>
+          <SectionHeader header="Browse new workouts" seeAll={true}/> 
+          <CategoryCardScroller/>
+        </div>
+      </div>
+      <NavBar activeButton="routines" />
     </div>
-    
-  )
-}
+  );
+};
 
 export default RoutinePage;
