6668342f3f9de7d081faefb500c0b4502c4f75bb
Krishab Bashyal <krishab2002@gmail.com>
2023-11-29 15:22:53 -0600

    pelase

--- src/components/CurrentSet.jsx
+++ src/components/CurrentSet.jsx
@@ -7,28 +7,28 @@ const CurrentSet = ({
   // You can add state and functions to handle the input changes if needed
   return (
     <div className='my-3'>
-      <div className="bg-gray-800 pb-5 pt-1 text-white rounded-lg max-w-sm mx-4 text-center">
+      <div className="bg-gray-800 pb-5 pt-1 rounded-lg max-w-sm mx-4 text-center">
       
         {/* Display exercises as a list */}
         <ul className="my-4">
-          {exercise && <li>{exercise}</li>}
+          {exercise && <li className='text-white'>{exercise}</li>}
         </ul>
         <div className="flex flex-row space-x-4 justify-center items-center">
           <section>
             <input
               type="number"
-              className="text-center w-16 rounded text p-1 mr-2"
+              className="text-center text-black w-16 rounded text p-1 mr-2"
               placeholder="X"
             />
-            <span className="rounded font-medium">Reps</span>
+            <span className="rounded font-medium text-white">Reps</span>
           </section>
           <section>
             <input
               type="number"
-              className="text-center w-16 rounded text p-1 mr-2"
+              className="text-center w-16 text-black rounded text p-1 mr-2"
               placeholder="X"
             />
-            <span className="rounded py-1 font-medium">Lbs</span>
+            <span className="rounded py-1 font-medium text-white">Lbs</span>
           </section>
         </div>
       
