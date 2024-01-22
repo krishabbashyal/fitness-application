9057779c7bae59bb1583059b51c63ee1b476e92b
Krishab Bashyal <krishab2002@gmail.com>
2024-01-21 21:01:39 -0600

    pushing my changes

--- src/pages/CreateWorkoutPage.jsx
+++ src/pages/CreateWorkoutPage.jsx
@@ -1,34 +1,54 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import SectionHeader from "../components/SectionHeader";
 
 import NavBar from "../components/NavBar";
-import SectionHeader from "../components/SectionHeader";
 
 const CreateWorkoutPage = () => {
   const [workoutName, setWorkoutName] = useState("");
+  const [exercise, setExerciseName] = useState("");
+  const [exercisesList, setExerciseList] = useState([]);
+
+  const addExercise = (newExercise) => {
+    setExerciseList([...exercisesList, newExercise]);
+  };
+
+  useEffect(() => {
+    console.log(exercisesList);
+  }, [exercisesList]);
 
   return (
     <div>
-      <div className="mx-4 mt-6 pb-[108px]">
-        <SectionHeader header="Create a new workout" />
-        <section>
-          <p className="mt-4">What should we call this workout?</p>
-          <input
-            className="w-full placeholder:text-[#8391A1] pl-3 h-[50px] rounded-lg mt-2 mb-5 border border-[#E8ECF4]"
-            type="text"
-            placeholder="Chest Day, Leg Day, etc."
-            value={workoutName}
-            onChange={(e) => setWorkoutName(e.target.value)}
-          />
-        </section>
-        <div className="flex justify-end space-x-2">
-        <button className="w-full bg-[#1E232C] text-white font-semibold h-14 rounded-lg mt-24" type="button">
-          <Link to={"/routines"} className="w-full h-14 bottom-0">
-            <p>Finish</p>
-          </Link>
-        </button>
-        <button className="w-full bg-[#475E88] text-white font-semibold h-14 rounded-lg mt-24" type="button">Add Workout</button>
+      <div className="mx-4 mt-6 flex flex-col">
+        <SectionHeader text="Your Training Plan" />
+        <input
+          type="text"
+          className="mt-4 h-16 placeholder:text-center placeholder:text-xl text-center text-2xl font-medium"
+          placeholder="Chest Day, Leg Day, etc."
+          value={workoutName}
+          onChange={(e) => setWorkoutName(e.target.value)}
+        />
       </div>
+      <div className="bg-slate-200 py-5 rounded-t-2xl relative bottom-0 w-full">
+        <p className="text-center font-medium">Add Exersises</p>
+        <div className="mx-4">
+          <section className="mt-4 flex flex-col">
+            <input className="h-11 text-center mb-4" type="text" placeholder="Exercise Title" value={exercise} onChange={(e) => setExerciseName(e.target.value)} />
+
+            <button className="bg-blue-400 text-white font-semibold h-11 rounded-lg w-full" onClick={() => addExercise(exercise)}>
+              Add Exercise
+            </button>
+            <div></div>
+            <section className="flex flex-row gap-1 mt-2 ">
+              <button className="bg-[#982F2F] w-full text-white font-semibold h-11 rounded-lg">
+                <Link to={"/routines"} className="w-full h-14">
+                  <p>Cancel</p>
+                </Link>
+              </button>
+              <button className="bg-[#475E88] w-full text-white font-semibold h-11 rounded-lg">Finish</button>
+            </section>
+          </section>
+        </div>
       </div>
       <NavBar activeButton="routines" />
     </div>
