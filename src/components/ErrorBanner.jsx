6aab7785b435cfd961daf2c49a00784f31efba0c
Krishab Bashyal <krishab2002@gmail.com>
2024-01-21 22:21:44 -0600

    Added ability to add and validate workouts in routines

--- src/components/ErrorBanner.jsx
+++ src/components/ErrorBanner.jsx
@@ -4,7 +4,7 @@ function ErrorBanner(props) {
   return (
     <div
       className=" flex justify-center items-center mb-3 rounded-md w-auto
-     h-12 bg-[#FFB1B1] shrink-0 "
+     h-11 bg-[#FFB1B1] shrink-0 "
     >
       <p className="text-[#991B1B]">{props.message}</p>
     </div>
--- src/pages/CreateWorkoutPage.jsx
+++ src/pages/CreateWorkoutPage.jsx
@@ -1,21 +1,47 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import SectionHeader from "../components/SectionHeader";
+import ErrorBanner from "../components/ErrorBanner";
 
 import NavBar from "../components/NavBar";
 
 const CreateWorkoutPage = () => {
   const [workoutName, setWorkoutName] = useState("");
   const [exercise, setExerciseName] = useState("");
+
   const [exercisesList, setExerciseList] = useState([]);
 
-  const addExercise = (newExercise) => {
-    setExerciseList([...exercisesList, newExercise]);
+  const [titleError, setTitleError] = useState("");
+  const [exerciseError, setExerciseError] = useState("");
+
+  const validateTitle = (title) => {
+    let cleanedTitle = title.replace(/\s+/g, " ").trim();
+    if (cleanedTitle.length == 0) {
+      setTitleError("Title can not be empty");
+    } else {
+      setTitleError("");
+    }
   };
 
-  useEffect(() => {
-    console.log(exercisesList);
-  }, [exercisesList]);
+  const validateExercise = (title) => {
+    let cleanedExercise = title.replace(/\s+/g, " ").trim();
+
+    if (exercisesList.includes(cleanedExercise)) {
+      setExerciseError("Exercise has already been added.")
+      return;
+    }
+
+    if (cleanedExercise.length == 0) {
+      setExerciseError("Cannot Add Empty Exercise");
+      return
+    } 
+
+    setExerciseList([...exercisesList, cleanedExercise]);
+    setExerciseName("");
+    setExerciseError("");
+    }
+
+
 
   return (
     <div>
@@ -23,19 +49,35 @@ const CreateWorkoutPage = () => {
         <SectionHeader text="Your Training Plan" />
         <input
           type="text"
-          className="mt-4 h-16 placeholder:text-center placeholder:text-xl text-center text-2xl font-medium"
+          className="mt-4 mb-2 h-12 placeholder:text-center placeholder:text-xl text-center text-2xl font-medium"
           placeholder="Chest Day, Leg Day, etc."
           value={workoutName}
           onChange={(e) => setWorkoutName(e.target.value)}
         />
+        {titleError.length > 0 && <ErrorBanner message={titleError} />}
       </div>
+
+      <div className="mx-4">
+        {exercisesList.length > 0 ? (
+          exercisesList.map((exercise, index) => (
+            <div key={index} className="p-2 text-center border rounded my-2">
+              {exercise}
+            </div>
+          ))
+        ) : (
+          <p className="text-center mt-36 text-gray-400">No exercises added yet</p>
+        )}
+      </div>
+
       <div className="bg-slate-200 py-5 rounded-t-2xl fixed bottom-20 w-full pb-8">
         <p className="text-center font-medium">Add Exersises</p>
         <div className="mx-4">
           <section className="mt-4 flex flex-col">
+            {exerciseError.length > 0 && <ErrorBanner message={exerciseError} />}
+
             <input className="h-11 text-center mb-4" type="text" placeholder="Exercise Title" value={exercise} onChange={(e) => setExerciseName(e.target.value)} />
 
-            <button className="bg-blue-400 text-white font-semibold h-11 rounded-lg w-full" onClick={() => addExercise(exercise)}>
+            <button className="bg-blue-400 text-white font-semibold h-11 rounded-lg w-full" onClick={() => validateExercise(exercise)}>
               Add Exercise
             </button>
             <section className="flex flex-row gap-1 mt-2 ">
@@ -44,7 +86,13 @@ const CreateWorkoutPage = () => {
                   <p>Cancel</p>
                 </Link>
               </button>
-              <button className="bg-[#475E88] w-full text-white font-semibold h-11 rounded-lg">Finish</button>
+              <button
+                className="bg-[#475E88] w-full text-white font-semibold h-11 rounded-lg"
+                onClick={() => {
+                  validateTitle(workoutName);
+                }}>
+                Finish
+              </button>
             </section>
           </section>
         </div>
